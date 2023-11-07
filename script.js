e209157a78c2b89a8f348202bb022320a8fb9400
Arham Sarwar <arhamsarwar786@gmail.com>
2023-11-07 06:38:18 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
+import * as THREE from "https://unpkg.com/three@0.149.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
 import { CSS2DRenderer, CSS2DObject } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/renderers/CSS2DRenderer.js';
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 2000);
-camera.position.set(0.5, 0.5, 1).setLength(14);
+camera.position.set(0.9, 0.5, 1).setLength(14);
 let renderer = new THREE.WebGLRenderer({
   antialias: true
 });
 renderer.setSize(innerWidth, innerHeight);
-renderer.setClearColor(0xffa1a1a1);
+renderer.setClearColor(0x000000);
 document.body.appendChild(renderer.domElement);
 
 
@@ -27,255 +27,149 @@ controls.minDistance = 6;
 controls.maxDistance = 15;
 controls.enableDamping = true;
 controls.autoRotate = true;
-controls.autoRotateSpeed *= 0.25;
+controls.autoRotateSpeed *= 0.95;
 
 let globalUniforms = {
   time: { value: 0 }
 };
 
-// <GLOBE>
-// https://web.archive.org/web/20120107030109/http://cgafaq.info/wiki/Evenly_distributed_points_on_sphere#Spirals
-let counter = 5000000;
 let rad = 5;
-let sph = new THREE.Spherical();
+const initialShellOpacity = 1.9; // Initial shell opacity
+const shellGeometry = new THREE.SphereBufferGeometry(rad + 0.2, 64, 64);
+const textureLoader1 = new THREE.TextureLoader();
+const texture3D1 = textureLoader1.load(imgData7); 
+const shellMaterial = new THREE.MeshPhongMaterial({
+  color: 0xffffff,
+  opacity: 1,
+  map: texture3D1,
+  transparent: true,
+  shininess: 0.0,
+})
 
-let r = 0;
-let dlong = Math.PI * (3 - Math.sqrt(5));
-let dz = 2 / counter;
-let long = 0;
-let z = 1 - dz / 2;
+const shell = new THREE.Mesh(shellGeometry, shellMaterial);
+scene.add(shell);
 
-let pts = [];
-let clr = [];
-let c = new THREE.Color();
-let uvs = [];
+// Create ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.03);
+scene.add(ambientLight);
 
-for (let i = 0; i < counter; i++) {
-  r = Math.sqrt(1 - z * z);
-  let p = new THREE.Vector3(
-    Math.cos(long) * r,
-    z,
-    -Math.sin(long) * r
-  ).multiplyScalar(rad);
-  pts.push(p);
-  z = z - dz;
-  long = long + dlong;
+// Create a hemisphere light
+// const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.0001);
+// scene.add(hemisphereLight);
 
-  c.setHSL(0.45, 0.5, Math.random() * 0.25 + 0.25);
-  c.toArray(clr, i * 3);
+// Example of adding directional lights to the scene
+const directionalLights = [];
 
-  sph.setFromVector3(p);
-  uvs.push((sph.theta + Math.PI) / (Math.PI * 2), 1.0 - sph.phi / Math.PI);
+// Add directional lights from different angles
+const light1 = new THREE.DirectionalLight(0xffffff, 0.1);
+light1.position.set(0, 0, 10); // Adjust the light direction
+directionalLights.push(light1);
+
+const light2 = new THREE.DirectionalLight(0xffffff, 0.1);
+light2.position.set(10, 0, 0); // Adjust the light direction
+directionalLights.push(light2);
+
+const light3 = new THREE.DirectionalLight(0xffffff, 0.1);
+light3.position.set(0, 10, 0); // Adjust the light direction
+directionalLights.push(light3);
+
+const light4 = new THREE.DirectionalLight(0xffffff, 0.1);
+light4.position.set(-10, 0, 0); // Adjust the light direction
+directionalLights.push(light4);
+
+const light5 = new THREE.DirectionalLight(0xffffff, 0.1);
+light5.position.set(0, -10, 0); // Adjust the light direction
+directionalLights.push(light5);
+
+// Add the lights to the scene
+for (const light of directionalLights) {
+  scene.add(light);
 }
 
-let g = new THREE.BufferGeometry().setFromPoints(pts);
-g.setAttribute("color", new THREE.Float32BufferAttribute(clr, 3));
-g.setAttribute("uv", new THREE.Float32BufferAttribute(uvs, 2));
-let m = new THREE.PointsMaterial({
-  size: 0.1,
-  vertexColors: false,  
-  color: new THREE.Color(0,0,0),
-  onBeforeCompile: (shader) => {
-    shader.uniforms.globeTexture = {
-      value: new THREE.TextureLoader().load(imgData)
-    };
-    shader.vertexShader = `
-    	uniform sampler2D globeTexture;
-      varying float vVisibility;
-      varying vec3 vNormal;
-      varying vec3 vMvPosition;
-      ${shader.vertexShader}
-    `.replace(
-      `gl_PointSize = size;`,
-      `
-      	vVisibility = texture(globeTexture, uv).g; // get value from texture
-        gl_PointSize = size * (vVisibility < 0.5 ? 1. : 0.75); // size depends on the value
-        vNormal = normalMatrix * normalize(position);
-        vMvPosition = -mvPosition.xyz;
-        gl_PointSize *= 0.4 + (dot(normalize(vMvPosition), vNormal) * 0.6); // size depends position in camera space
-      `
-    );
-    //console.log(shader.vertexShader);
-    shader.fragmentShader = `
-    	varying float vVisibility;
-      varying vec3 vNormal;
-      varying vec3 vMvPosition;
-      ${shader.fragmentShader}
-    `.replace(
-      `vec4 diffuseColor = vec4( diffuse, opacity );`,
-      `
-      	bool circ = length(gl_PointCoord - 0.5) > 0.5; // make points round
-        bool vis = dot(vMvPosition, vNormal) < 0.; // visible only on the front side of the sphere
-      	if (circ || vis) discard;
-        
-        vec3 col = diffuse + (vVisibility > 0.5 ? 0.5 : 0.); // make oceans brighter
-        
-        vec4 diffuseColor = vec4( col, opacity );
-      `
-    );
-    //console.log(shader.fragmentShader);
-  }
+let galaxyGeometry = new THREE.SphereGeometry(100, 32, 32);
+let galaxyMaterial = new THREE.MeshBasicMaterial({
+  side: THREE.BackSide
 });
-let globe = new THREE.Points(g, m);
-scene.add(globe);
-
-  // <ICOSAHEDRON>
-  let icshdrn = new THREE.Mesh(new THREE.IcosahedronGeometry(rad, 1), new THREE.MeshBasicMaterial({color: 0x647f7f, wireframe: true}));
-  globe.add(icshdrn);
-  // </ICOSAHEDRON>
-// </GLOBE>
-
-const db = firebase.firestore();
-
+let galaxy = new THREE.Mesh(galaxyGeometry, galaxyMaterial);
+const textureLoader = new THREE.TextureLoader();
+// Load Galaxy Textures
+textureLoader.crossOrigin = true;
+textureLoader.load(
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/starfield.png',
+  function(texture) {
+    galaxyMaterial.map = texture;
+    scene.add(galaxy);
+  }
+);
+  const db = firebase.firestore();
+  
 var list = await db.collection("users")
   .get();
 
   console.log(list.docs.length);
-const markerCount = list.docs.length;
-let markerInfo = []; // information on markers
-let gMarker = new THREE.PlaneGeometry();
-let mMarker = new THREE.MeshBasicMaterial({
-  color: 0xffeec23f  ,
-  onBeforeCompile: (shader) => {
-    shader.uniforms.time = globalUniforms.time;
-    shader.vertexShader = `
-    	attribute float phase;
-      varying float vPhase;
-      ${shader.vertexShader}
-    `.replace(
-      `#include <begin_vertex>`,
-      `#include <begin_vertex>
-      	vPhase = phase; // de-synch of ripples
-      `
-    );
-    //console.log(shader.vertexShader);
-    shader.fragmentShader = `
-    	uniform float time;
-      varying float vPhase;
-    	${shader.fragmentShader}
-    `.replace(
-      `vec4 diffuseColor = vec4( diffuse, opacity );`,
-      `
-      vec2 lUv = (vUv - 0.5) * 2.;
-      float val = 0.;
-      float lenUv = length(lUv);
-      val = max(val, 1. - step(0.01, lenUv)); // central circle
-      val = max(val, step(0.05, lenUv) - step(0.05, lenUv)); // outer circle
-      
-      float tShift = fract(time * 0.5 + vPhase);
-      val = max(val, step(0.1 + (tShift * 0.05), lenUv) - step(0.08 + (tShift * 0.2), lenUv)); // ripple
-      
-      if (val < 0.1) discard;
-      
-      vec4 diffuseColor = vec4( diffuse, opacity );`
-    );
-    //console.log(shader.fragmentShader)
-  }
-});
-mMarker.defines = { USE_UV: " " }; // needed to be set to be able to work with UVs
-let markers = new THREE.InstancedMesh(gMarker, mMarker, markerCount);
-
-let dummy = new THREE.Object3D();
-let phase = [];
-for (let i = 0; i < markerCount; i++) {
-  dummy.position.randomDirection().setLength(rad + 0.1);
-  dummy.lookAt(dummy.position.clone().setLength(rad + 1));
-  dummy.updateMatrix();
-  markers.setMatrixAt(i, dummy.matrix);
-  phase.push(Math.random());
-
-  markerInfo.push({
-    id: i + 1,
-    mag: THREE.MathUtils.randInt(1, 10),
-    crd: dummy.position.clone()
-  });
-}
-gMarker.setAttribute(
-  "phase",
-  new THREE.InstancedBufferAttribute(new Float32Array(phase), 1)
-);
-
-scene.add(markers);
-// </Markers>
-
-// <Label>
-let labelDiv = document.getElementById("markerLabel");
+  const markerCount = 30;
+  let labelDiv = document.getElementById("markerLabel");
 let closeBtn = document.getElementById("closeButton");
 closeBtn.addEventListener("pointerdown", event => {
   labelDiv.classList.add("hidden");
 })
-let label = new CSS2DObject(labelDiv);
-label.userData = {
-  cNormal: new THREE.Vector3(),
-  cPosition: new THREE.Vector3(),
-  mat4: new THREE.Matrix4(),
-  trackVisibility: () => { // the closer to the edge, the less opacity
-    let ud = label.userData;
-    ud.cNormal.copy(label.position).normalize().applyMatrix3(globe.normalMatrix);
-    ud.cPosition.copy(label.position).applyMatrix4(ud.mat4.multiplyMatrices(camera.matrixWorldInverse, globe.matrixWorld));
-    let d = ud.cPosition.negate().normalize().dot(ud.cNormal);
-    d = smoothstep(0.2, 0.7, d);
-    label.element.style.opacity = d;
-    
-    // https://github.com/gre/smoothstep/blob/master/index.js
-    function smoothstep (min, max, value) {
-      var x = Math.max(0, Math.min(1, (value-min)/(max-min)));
-      return x*x*(3 - 2*x);
-    };
+
+addRandomSpotlights(140, Math.PI / 15);
+
+function addSpotLight(x,y,z){
+  const spotLight = new THREE.SpotLight( 0xffffff );
+  spotLight.position.set( x,y,z );
+  spotLight.castShadow = false;
+  spotLight.intensity = 1;
+  spotLight.angle = Math.PI / 15;
+  spotLight.penumbra = 0.06; 
+   spotLight.distance = 10;
+  const helper = new THREE.SpotLightHelper(spotLight);
+  // scene.add(helper);
+  
+  scene.add( spotLight );
+  
+}
+
+function addRandomSpotlights(numSpotlights) {
+  const minRadius = 0.5; // Minimum radius
+  const maxRadius = 0.5; // Maximum radius
+  const minSize = 0.2; // Minimum spotlight size
+  const maxSize = 0.2; // Maximum spotlight size
+
+  for (let i = 0; i < numSpotlights; i++) {
+    // Generate random positions outside the globe's surface
+    const radius = Math.random() * (maxRadius - minRadius) + minRadius;
+    const size = Math.random() * (maxSize - minSize) + minSize;
+    const theta = Math.random() * Math.PI * 2; // Random angle
+    const phi = Math.random() * Math.PI; // Random elevation angle
+
+    // Calculate the spotlight positions outside the globe
+    const globeRadius = 5; // Globe radius
+    const x = (globeRadius + radius) * Math.sin(phi) * Math.cos(theta);
+    const y = (globeRadius + radius) * Math.sin(phi) * Math.sin(theta);
+    const z = (globeRadius + radius) * Math.cos(phi);
+
+    addSpotLight(x, y, z, size);
   }
 }
-scene.add(label);
-// </Label>
-
-// <Interaction>
-let pointer = new THREE.Vector2();
-let raycaster = new THREE.Raycaster();
-let intersections;
-let divID = document.getElementById("idNum");
-let divMag = document.getElementById("magnitude");
-let divCrd = document.getElementById("coordinates");
-window.addEventListener("pointerdown", event => {
-  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  raycaster.setFromCamera(pointer, camera);
-  intersections = raycaster.intersectObject(markers).filter(m => {
-    return (m.uv.subScalar(0.5).length() * 2) < 0.25; // check, if we're in the central circle only
-  });
-  //console.log(intersections);
-  if (intersections.length > 0) {
-    let iid = intersections[0].instanceId;
-    let mi = markerInfo[iid];
-    divID.innerHTML = `ID: <b>${mi.id}</b>`;
-    divMag.innerHTML = `Mag: <b>${mi.mag}</b>`;
-    divCrd.innerHTML = `X: <b>${mi.crd.x.toFixed(2)}</b>; Y: <b>${mi.crd.y.toFixed(2)}</b>; Z: <b>${mi.crd.z.toFixed(2)}</b>`;
-    label.position.copy(mi.crd);
-    label.element.animate([
-      {width: "0px", height: "0px", marginTop: "0px", marginLeft: "0px"},
-      {width: "230px", height: "50px", marginTop: "-25px", maginLeft: "120px"}
-    ],{
-      duration: 250
-    });
-    label.element.classList.remove("hidden");
-  }
-  
-})
-// </Interaction>
 
 let clock = new THREE.Clock();
+
 
 renderer.setAnimationLoop(() => {
   let t = clock.getElapsedTime();
   globalUniforms.time.value = t;
-  label.userData.trackVisibility();
+  renderer.shadowMap.enabled = false;
   controls.update();
   renderer.render(scene, camera);
   labelRenderer.render(scene, camera);
 });
 
+console.log("errors")
 function onWindowResize() {
   camera.aspect = innerWidth / innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(innerWidth, innerHeight);
   labelRenderer.setSize(innerWidth, innerHeight);
 }
